616d701292515b9b33e1db73343a305439e08839
mygit-sampath-govardhan <111648450+mygit-sampath-govardhan@users.noreply.github.com>
2022-08-24 10:40:02 +0530

    Add files via upload

---
+++ Assignment1-code.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void disp_7447(int C, int B, int A)
+{
+  digitalWrite(2, A); 
+  digitalWrite(3, B); 
+  digitalWrite(4, C); 
+}
+void setup() {
+    pinMode(2, OUTPUT);  
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+}
+void loop() {
+disp_7447(1,1,0);  
+}
+
